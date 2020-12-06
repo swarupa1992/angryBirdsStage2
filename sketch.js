790266ff15b2a708698db0a815bb6be6a43afa54
swarupa1992 <64201669+swarupa1992@users.noreply.github.com>
2020-12-06 22:25:11 +0530

    Update sketch.js
    pl added

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
 
-var engine, world;
+var engine, world,platform;
 var box1, pig1;
 var backgroundImg;
 
@@ -16,7 +16,8 @@ function setup(){
     world = engine.world;
 
     
-    ground = new Ground(600,height,1200,20)
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(150,300,300,170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -34,6 +35,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    
 
 }
 
@@ -59,4 +61,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
 }
